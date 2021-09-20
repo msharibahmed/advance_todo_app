e42d13db12a2e8833fb2452b80c144ea8ba4177f
Sharib <msharibahmed@gmail.com>
2021-09-20 16:45:50 +0530

    minor changes

--- lib/widgets/google_signin_btn.dart
+++ lib/widgets/google_signin_btn.dart
@@ -3,7 +3,6 @@ import 'package:advance_todo_app/screens/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 //
-import 'package:advance_todo_app/screens/home_screen.dart';
 //
 import 'package:advance_todo_app/utils/firebase_initialization.dart';
 
