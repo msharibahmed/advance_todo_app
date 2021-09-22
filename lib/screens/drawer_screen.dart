03f826457b081eeb38e96184a933b1efd98f0cfe
Sharib <msharibahmed@gmail.com>
2021-09-22 22:34:29 +0530

    added drawer screen and UI with logged in user info

--- lib/screens/drawer_screen.dart
+++ lib/screens/drawer_screen.dart
@@ -1,20 +1,19 @@
+import 'package:advance_todo_app/provider/user_prov.dart';
 import 'package:advance_todo_app/utils/constants.dart';
+import 'package:advance_todo_app/utils/firebase_initialization.dart';
 import 'package:advance_todo_app/widgets/drawer_tile_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 
-class DrawerScreen extends StatefulWidget {
+class DrawerScreen extends StatelessWidget {
   static const routeName = '/drawer-screen';
   const DrawerScreen({Key? key}) : super(key: key);
 
   @override
-  State<DrawerScreen> createState() => _DrawerScreenState();
-}
-
-class _DrawerScreenState extends State<DrawerScreen> {
-  @override
   Widget build(BuildContext context) {
+    final _userInfo = Provider.of<UserInfoProv>(context).loggedInUserInfo;
     return Scaffold(
       backgroundColor: darkbackgroundColor,
       body: Padding(
@@ -27,35 +26,46 @@ class _DrawerScreenState extends State<DrawerScreen> {
               height: 200,
             ),
             CircleAvatar(
-              child: Image.asset(
-                'assets/images/logo.png',
-              ),
+              backgroundImage:
+                  NetworkImage(_userInfo!.photoURL ?? 'assets/images/logo.png'),
               maxRadius: 50,
               minRadius: 50,
             ),
             const SizedBox(height: 40),
-            const Text(
-              'Sharib\nAhmed',
-              style: TextStyle(
+            Text(
+              _userInfo.displayName ?? "New User",
+              style: const TextStyle(
                   color: Colors.white,
                   fontSize: 40,
                   fontWeight: FontWeight.w300),
             ),
             const SizedBox(height: 60),
-            const DrawerListTileWidget(
-                title: 'Today', icon: Icons.turned_in_not),
+            DrawerListTileWidget(
+              title: 'Today',
+              icon: Icons.turned_in_not,
+              func: () {},
+            ),
             const SizedBox(height: 5),
-            const DrawerListTileWidget(
-                title: 'Upcoming', icon: Icons.task_outlined),
+            DrawerListTileWidget(
+                title: 'Upcoming', icon: Icons.task_outlined, func: () {}),
             const SizedBox(height: 5),
-            const DrawerListTileWidget(
-                title: 'Completed', icon: Icons.assignment),
+            DrawerListTileWidget(
+                title: 'Completed', icon: Icons.assignment, func: () {}),
             const SizedBox(height: 5),
-            const DrawerListTileWidget(title: 'Settings', icon: Icons.settings),
+            DrawerListTileWidget(
+              title: 'Settings',
+              icon: Icons.settings,
+              func: () {},
+            ),
             const SizedBox(height: 5),
             const Spacer(),
-            const DrawerListTileWidget(
-                title: 'Log Out', icon: Icons.logout_rounded),
+            DrawerListTileWidget(
+              title: 'Log Out',
+              icon: Icons.logout_rounded,
+              func: () async {
+                await Authentication.signOut(context: context);
+              },
+            ),
           ],
         ),
       ),
--- lib/widgets/drawer_tile_widget.dart
+++ lib/widgets/drawer_tile_widget.dart
@@ -3,22 +3,28 @@ import 'package:flutter/material.dart';
 class DrawerListTileWidget extends StatelessWidget {
   final String title;
   final IconData icon;
+  final Function func;
 
   const DrawerListTileWidget(
-      {Key? key, required this.title, required this.icon})
+      {Key? key, required this.title, required this.icon, required this.func})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Icon(
-        icon,
-        color: Colors.grey,
-      ),
-      title: Text(
-        title,
-        style: const TextStyle(
-            color: Colors.white, fontSize: 20, fontWeight: FontWeight.w300),
+    return GestureDetector(
+      onTap: () async {
+        await func();
+      },
+      child: ListTile(
+        leading: Icon(
+          icon,
+          color: Colors.grey,
+        ),
+        title: Text(
+          title,
+          style: const TextStyle(
+              color: Colors.white, fontSize: 20, fontWeight: FontWeight.w300),
+        ),
       ),
     );
   }
